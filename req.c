57bb8d71d570ade65c8d62b526fb6f88c8c26906
James Punnett <jppunnett@yahoo.com>
2017-01-12 06:14:13 -0400

    Get http status code and response time from libcurl

--- req.c
+++ req.c
@@ -42,15 +42,21 @@ MakeRequest(const char* url, int timeout, struct reqstats *rsp)
         /* Be a friendly Web citizen and declare who is making the request */
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "dboom/1.0");
         /* Perform the request, res will get the return code */
-        int64_t start = now();
         res = curl_easy_perform(curl);
-        rsp->tm = now() - start;
         if(res != CURLE_OK) {
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
             rc = -1;
-        }
+        } else {
+            long http_code = 0;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+            rsp->http_code = http_code;
 
+            double reqtime = 0;
+            curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &reqtime);
+            /* Convert from curl's unit of measure (seconds) to milliseconds */
+            rsp->tm = (int64_t)(reqtime * 1000);
+        }
         curl_easy_cleanup(curl);
     }
     return rc;
